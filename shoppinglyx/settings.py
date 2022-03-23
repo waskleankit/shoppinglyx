2f41655f1404a101ae057b4e0cc867f430048bec
waskleankit <ankitwaskle143@gmail.com>
2022-03-23 12:51:46 +0530

    heroku change static root

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/profile/'

2c0cdb1c8b7215ab674b89fe0dba8f1a6addfca8
waskleankit <ankitwaskle143@gmail.com>
2022-03-23 13:04:21 +0530

    heroku change static root  change

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 
 # STATIC_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'

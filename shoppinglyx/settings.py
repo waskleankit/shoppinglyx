8c361bcc6f43c5a8e0dc47485dc10465e92809c3
waskleankit <ankitwaskle143@gmail.com>
2022-03-23 13:55:28 +0530

    staticfiles dir

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-CSRF_COOKIE_SECURE = None
+# CSRF_COOKIE_SECURE = None
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -129,9 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATICFILES_DIRS = (
-#      os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+)
 # STATIC_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 MEDIA_URL = '/media/'
